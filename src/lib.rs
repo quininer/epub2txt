6b8f235e722163f577add45525fbb0661c126c9a
quininer kel <quininer@live.com>
2017-03-11 10:57:00 +0800

    [Changed] use sanngaa

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
-#![feature(field_init_shorthand)]
-
 extern crate url;
 extern crate zip;
-extern crate kuchiki;
+extern crate sanngaa;
 extern crate html2text;
 #[macro_use] extern crate error_chain;
 
@@ -12,8 +10,8 @@ use std::path::{ Path, PathBuf };
 use std::io::{ self, Read, Write, Seek };
 use url::percent_encoding::percent_decode;
 use zip::ZipArchive;
-use kuchiki::traits::*;
-use kuchiki::{ NodeDataRef, ElementData };
+use sanngaa::traits::*;
+use sanngaa::{ NodeDataRef, ElementData };
 pub use error::{ Error, ErrorKind };
 
 
@@ -23,6 +21,7 @@ impl<T: Read + Seek> ReadSeek for T {}
 
 #[derive(Debug)]
 pub struct Book<R: ReadSeek> {
+    /// epub zip archive
     pub epub: ZipArchive<R>,
     /// `(title, author, description)`
     pub metadata: (String, String, String),
@@ -35,33 +34,33 @@ impl<R: ReadSeek> Book<R> {
         let mut root = Path::new(opf).to_path_buf();
         root.pop();
 
-        let dom = kuchiki::parse_html()
+        let dom = sanngaa::parse_xml()
             .from_utf8()
             .read_from(&mut epub.by_name(opf)?)?;
 
         let metadata = (
             dom.select(r"dc\:title").unwrap()
-                .next().map(|e| e.text_contents())
+                .next().and_then(|e| e.as_node().first_child())
+                .and_then(|e| e.as_text().map(|t| t.borrow().to_string()))
                 .ok_or("No found <dc:title>.")?,
             dom.select(r"dc\:creator").unwrap()
-                .next().map(|e| e.text_contents())
+                .next().and_then(|e| e.as_node().first_child())
+                .and_then(|e| e.as_text().map(|t| t.borrow().to_string()))
                 .unwrap_or_else(|| String::from("anonymous")),
             dom.select(r"dc\:description").unwrap()
-                .next().map(|e| e.text_contents())
+                .next().and_then(|e| e.as_node().first_child())
+                .and_then(|e| e.as_text().map(|t| t.borrow().to_string()))
                 .unwrap_or_else(|| String::from("None"))
         );
 
         let ncx_node = dom.select("item#ncx, item#toc, item#ncxtoc").unwrap()
             .next().ok_or("No found <item id=..>.")?;
-        let ncx_attr = ncx_node
-            .as_node().as_element().ok_or("No element.")?
-            .attributes.borrow();
-        let ncx_path = ncx_attr
+        let ncx_path = ncx_node.attributes.borrow()
             .get("href")
             .map(|p| root.join(p))
             .ok_or("No `href` in opf.")?;
 
-        let mut nav = kuchiki::parse_html()
+        let mut nav = sanngaa::parse_xml()
             .from_utf8()
             .read_from(&mut epub.by_name(ncx_path.to_str().unwrap())?)?
             .select("navMap > navPoint").unwrap()
@@ -69,7 +68,7 @@ impl<R: ReadSeek> Book<R> {
             .filter_map(|r| match r {
                 Ok(output) => Some(output),
                 Err(err) => {
-                    write!(io::stderr(), "warn: {}", err).unwrap();
+                    writeln!(io::stderr(), "warn: {}", err).unwrap();
                     None
                 }
             })
@@ -82,17 +81,14 @@ impl<R: ReadSeek> Book<R> {
     }
 
     pub fn from_container(mut epub: ZipArchive<R>) -> Result<Book<R>, Error> {
-        let node = kuchiki::parse_html()
+        let node = sanngaa::parse_xml()
             .from_utf8()
             .read_from(&mut epub.by_name("META-INF/container.xml")?)?
             .select("rootfile").unwrap()
             .next().ok_or("No found <rootfile>.")?;
 
-        let attr = node
-            .as_node().as_element().ok_or("No element.")?
-            .attributes.borrow();
-
-        Book::new(epub, attr.get("full-path").ok_or("No `full-path` in container.")?)
+        let attrs = node.attributes.borrow();
+        Book::new(epub, attrs.get("full-path").ok_or("No `full-path` in container.")?)
     }
 
     pub fn write_to(&mut self, output: &mut Write) -> Result<(), Error> {
@@ -113,7 +109,7 @@ impl<R: ReadSeek> Book<R> {
             let path = percent_decode(path.as_bytes()).decode_utf8()?;
             write!(
                 output,
-                "{}\n{}\n\n",
+                "{}:\n{}\n\n",
                 label.trim(),
                 html2text::from_read(&mut self.epub.by_name(&path)?, 180),
             )?;
@@ -124,24 +120,19 @@ impl<R: ReadSeek> Book<R> {
 }
 
 fn node_get_nav(root: &Path, node: NodeDataRef<ElementData>) -> Result<(usize, String, PathBuf), Error> {
-    let node = node.as_node();
-
-    let order = node.as_element().ok_or("No element.")?
-        .attributes.borrow()
-        .get("playorder").ok_or("No `playOrder` in <navPoint>")?
+    let order = node.attributes.borrow()
+        .get("playOrder").ok_or("No `playOrder` in <navPoint>")?
         .parse::<usize>()?;
 
-    let label = node.select("navLabel > text").unwrap()
+    let label = node.as_node().select("navLabel > text").unwrap()
         .next().ok_or("No found <text>.")?
         .text_contents();
 
-    let path = node.select("content").unwrap()
+    let path = node.as_node().select("content").unwrap()
         .next()
-        .and_then(|n| n.as_node().as_element()
-            .and_then(|n| n.attributes.borrow()
-                .get("src")
-                .map(|src| root.join(src))
-            )
+        .and_then(|n| n.attributes.borrow()
+            .get("src")
+            .map(|src| root.join(src))
         )
         .ok_or("No found <content> or `src`.")?;
 
--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,39 @@ extern crate epub2txt;
 
 use std::io::{ self, Read, Write, Cursor };
 use std::fs::File;
-use clap::{ Arg, App };
+use clap::{ Arg, App, ArgMatches };
 use epub2txt::{ ReadSeek, epub2txt, Error, ErrorKind };
 
 
-fn main() {
-    let matches = App::new(env!("CARGO_PKG_NAME"))
+fn app() -> App<'static, 'static> {
+    App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .arg(Arg::with_name("input").value_name("INPUT"))
         .arg(Arg::with_name("output").short("o").long("output").value_name("OUTPUT").help("write to file."))
-        .get_matches();
+}
 
-    match epub2txt(
-        &mut if let Some(path) = matches.value_of("input") {
-            Box::new(File::open(path).unwrap()) as Box<ReadSeek>
-        } else {
-            let mut input = Vec::new();
-            io::stdin().read_to_end(&mut input).unwrap();
-            Box::new(Cursor::new(input)) as Box<ReadSeek>
-        },
-        &mut if let Some(path) = matches.value_of("output") {
-            Box::new(File::create(path).unwrap()) as Box<Write>
-        } else {
-            Box::new(io::stdout()) as Box<Write>
-        }
-    ) {
-        Err(Error(ErrorKind::Io(ref err), _)) if err.kind() == io::ErrorKind::BrokenPipe => (),
-        err => err.unwrap()
+fn start<'a>(matches: ArgMatches<'a>) -> Result<(), Error> {
+    let mut reader = if let Some(path) = matches.value_of("input") {
+        Box::new(File::open(path)?) as Box<ReadSeek>
+    } else {
+        let mut input = Vec::new();
+        io::stdin().read_to_end(&mut input)?;
+        Box::new(Cursor::new(input)) as Box<ReadSeek>
     };
+    let mut writer = if let Some(path) = matches.value_of("output") {
+        Box::new(File::create(path)?) as Box<Write>
+    } else {
+        Box::new(io::stdout()) as Box<Write>
+    };
+
+    match epub2txt(&mut reader, &mut writer) {
+        Err(Error(ErrorKind::Io(ref err), _)) if err.kind() == io::ErrorKind::BrokenPipe => Ok(()),
+        output => output
+    }
+}
+
+fn main() {
+    start(app().get_matches()).unwrap();
 }
