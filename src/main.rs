ade7e03bc9f80f82f9eb4d6fc8b2aa71629fb72c
leviking <lvikng56@gmail.com>
2024-01-24 16:21:48 -0600

    chore: update clap

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,31 @@ extern crate clap;
 extern crate failure;
 extern crate epub2txt;
 
-use std::io::{ self, Read, Write, Cursor };
+use std::io::{self, Read, Write, Cursor};
 use std::fs::File;
-use clap::{ Arg, App, ArgMatches };
+use clap::{Arg, Command, ArgMatches};
 use failure::Error;
-use epub2txt::{ ReadSeek, epub2txt };
-
+use epub2txt::{ReadSeek, epub2txt};
 
 #[inline]
-fn app() -> App<'static, 'static> {
-    App::new(env!("CARGO_PKG_NAME"))
+fn app() -> Command {
+    Command::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
-        .arg(Arg::with_name("input").value_name("INPUT"))
-        .arg(Arg::with_name("output").short("o").long("output").value_name("OUTPUT").help("write to file."))
+        .arg(Arg::new("input").value_name("INPUT"))
+        .arg(Arg::new("output").short('o').long("output").value_name("OUTPUT").help("write to file."))
 }
 
 fn start(matches: &ArgMatches) -> Result<(), Error> {
-    let mut reader = if let Some(path) = matches.value_of("input") {
+    let mut reader = if let Some(path) = matches.get_one::<String>("input").cloned() {
         Box::new(File::open(path)?) as Box<dyn ReadSeek>
     } else {
         let mut input = Vec::new();
         io::stdin().read_to_end(&mut input)?;
         Box::new(Cursor::new(input)) as Box<dyn ReadSeek>
     };
-    let mut writer = if let Some(path) = matches.value_of("output") {
+    let mut writer = if let Some(path) = matches.get_one::<String>("output").cloned() {
         Box::new(File::create(path)?) as Box<dyn Write>
     } else {
         Box::new(io::stdout()) as Box<dyn Write>
